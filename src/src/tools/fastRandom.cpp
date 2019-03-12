268f5451aada4c7327fa39cfb14f181d65cb422c
BrutPitt <brutpitt@gmail.com>
2019-03-12 23:38:37 +0100

    1.1.2 WiP Commit II - Copyright update

--- src/src/tools/fastRandom.cpp
+++ src/src/tools/fastRandom.cpp
@@ -1,10 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2018 Michele Morrone
+//  All rights reserved.
+//
+//  mailto:me@michelemorrone.eu
+//  mailto:brutpitt@gmail.com
+//  
+//  https://github.com/BrutPitt
+//
+//  https://michelemorrone.eu
+//  https://BrutPitt.com
+//
+//  This software is distributed under the terms of the BSD 2-Clause license:
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//   
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+//  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+//  From George Marsaglia Algorithms <geo@stat.fsu.edu> 
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include "fastRandom.h"
 
 fastRandomClass fastRandom;
 
 uint32_t xorshift32()
 {
-	/* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
+	// Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" 
     static uint32_t state = fastRandom.rnd32();
 	uint32_t x = state;
 	x ^= x << 13;
@@ -16,6 +56,7 @@ uint32_t xorshift32()
 
 uint64_t xorshift64()
 {
+    // Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" 
     static size_t state = fastRandom.rnd64();
 	uint64_t x = state;
 	x^= x << 13;
