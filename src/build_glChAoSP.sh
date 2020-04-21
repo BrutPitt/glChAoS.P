4c7094eafabeabe38dc66c6856b685f3712869f0
BrutPitt <brutpitt@gmail.com>
2020-04-21 23:00:49 +0200

    ver. 1.5 WiP - Supports MinGW/Clang in Windows, using also in VS2019 - now is need CMake >= v.3.15

--- src/build_glChAoSP.sh
+++ src/build_glChAoSP.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 echo ""
-echo "    start to build Particles..."
+echo "    glChAoS.p build starts . . ."
 echo ""
+echo "    ---  CMake stage  ---"
 echo ""
 
-cmake -DBUILD_wglChAoSP:BOOL=FALSE -G Unix\ Makefiles -DCMAKE_BUILD_TYPE:String=Release
+BUILD_DIR=cmake-build-Release
+cmake -DBUILD_wglChAoSP:BOOL=FALSE -G Unix\ Makefiles -DCMAKE_BUILD_TYPE:String=Release -B$BUILD_DIR
 #-DOpenGL_GL_PREFERENCE="GLVND"
 #-DCMAKE_EXE_LINKER_FLAGS="-static"
+echo ""
+echo "    ---  make stage  ---"
+echo ""
+echo ""
+
+cd $BUILD_DIR
 make -j 8
+cd ..
 
 echo ""
-echo "    ... end Particles"
+echo "    . . . end glChAoS.P"
 echo ""
 echo ""
