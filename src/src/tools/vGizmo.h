2b0669b01e4929dc7b780a1df1ba4d9fee5ae99c
BrutPitt <brutpitt@gmail.com>
2020-05-16 07:03:17 +0200

    ver. 1.5 Release (WiP) - vGizmo 3.0 ==> https://github.com/BrutPitt/virtualGizmo3D

--- src/src/tools/vGizmo.h
+++ src/src/tools/vGizmo.h
@@ -163,8 +163,8 @@ public:
         T AdotB = dot(a, b); 
         T angle = acos( AdotB>T(1) ? T(1) : (AdotB<-T(1) ? -T(1) : AdotB)); // clamp need!!! corss float is approximate to FLT_EPSILON
 
-        qtStep = normalize(angleAxis(angle * tbScale * fpsRatio, axis * rotationVector));
-        qtIdle = normalize(angleAxis(angle * tbScale * fpsRatio * qStepRatio, axis * rotationVector* qStepRatio));
+        qtStep = normalize(angleAxis(angle * tbScale * fpsRatio                  , axis * rotationVector));
+        qtIdle = normalize(angleAxis(angle * tbScale * fpsRatio * qIdleSpeedRatio, axis * rotationVector));
         qtV = qtStep*qtV;
 
     }
@@ -226,6 +226,17 @@ public:
     //////////////////////////////////////////////////////////////////
     void setStepRotation(const tQuat &q) { qtStep = q; }
 
+    // attenuation<1.0 / increment>1.0 of rotation speed in idle
+    ////////////////////////////////////////////////////////////////////////////
+    void setIdleRotSpeed(T f) { qIdleSpeedRatio = f;    }
+    T    getIdleRotSpeed()    { return qIdleSpeedRatio; }
+
+    //  return current transformations as 4x4 matrix.
+    ////////////////////////////////////////////////////////////////////////////
+    virtual tMat4 getTransform() = 0;
+    ////////////////////////////////////////////////////////////////////////////
+    virtual void applyTransform(tMat4 &model) = 0;
+
 // Immediate mode helpers
 //////////////////////////////////////////////////////////////////////
 
@@ -249,15 +260,6 @@ public:
         update();
     }
 
-    //  return current transformations as 4x4 matrix.
-    ////////////////////////////////////////////////////////////////////////////
-    virtual tMat4 getTransform() = 0;
-    ////////////////////////////////////////////////////////////////////////////
-    virtual void applyTransform(tMat4 &model) = 0;
-
-    void setRotStepRatio(T f) { qStepRatio = f; }
-    T    getRotStepRatio()    { return qStepRatio; }
-
 protected:
 
     tVec2 pos, delta;
@@ -278,7 +280,7 @@ protected:
     //////////////////////////////////////////////////////////////////
     T tbScale = T(1);    //base scale sensibility
     T fpsRatio = T(1);   //auto adjust by FPS (call idle with current FPS)
-    T qStepRatio = T(1); //autoRotation factor to speedup/slowdown
+    T qIdleSpeedRatio = T(.33); //autoRotation factor to speedup/slowdown
     
     T minVal;
     tVec3 offset;
@@ -380,7 +382,7 @@ public:
     ////////////////////////////////////////////////////////////////////////////
     void wheel( T x, T y, T z=T(0)) {
         povPanDollyFactor = z;
-        dolly.z += y * dollyScale * T(10) * (povPanDollyFactor>T(0) ? povPanDollyFactor : T(1));
+        dolly.z += y * dollyScale * wheelScale * (povPanDollyFactor>T(0) ? povPanDollyFactor : T(1));
     }
 
     //////////////////////////////////////////////////////////////////
@@ -453,18 +455,30 @@ public:
     int getPanControlButton() { return panControlButton; }
     int getPanControlModifier() { return panControlModifiers; }
 
-    //  Set the speed for the dolly operation.
+    // Sensitivity for Wheel movements -> Normalized: less < 1 < more
     //////////////////////////////////////////////////////////////////
-    void setWheelScale( T scale) { wheelScale = scale;  }
-    T getWheelScale() { return wheelScale;  }
-    //  Set the speed for the dolly operation.
+    void setNormalizedWheelScale( T scale) { wheelScale = scale*constWheelScale;  }
+    void setWheelScale( T scale)           { wheelScale = scale;  }
+    T getNormalizedWheelScale() { return wheelScale/constWheelScale;  }
+    T getWheelScale()           { return wheelScale;  }
+    // Sensitivity for Dolly movements -> Normalized: less < 1 < more
     //////////////////////////////////////////////////////////////////
-    void setDollyScale( T scale) { dollyScale = scale;  }
-    T getDollyScale() { return dollyScale;  }
-    //  Set the speed for pan
+    void setNormalizedDollyScale( T scale) { dollyScale = scale*constDollyScale;  }
+    void setDollyScale( T scale)           { dollyScale = scale;  }
+    T getNormalizedDollyScale() { return dollyScale/constDollyScale;  }
+    T getDollyScale()           { return dollyScale;  }
+    // Sensitivity for Pan movements -> Normalized: less < 1 < more
     //////////////////////////////////////////////////////////////////
-    void setPanScale( T scale) { panScale = scale; }
-    T getPanScale() { return panScale; }
+    void setNormalizedPanScale( T scale) { panScale = scale*constPanScale; }
+    void setPanScale( T scale)           { panScale = scale; }
+    T getNormalizedPanScale() { return panScale/constPanScale; }
+    T getPanScale()           { return panScale; }
+    // Sensitivity for pan/dolly by distance -> Normalized: less < 1 < more
+    //////////////////////////////////////////////////////////////////
+    void setNormalizedDistScale( T scale) { distScale = scale*constDistScale; }
+    void setDistScale( T scale)           { distScale = scale; }
+    T getNormalizedDistScale() { return distScale/constDistScale; }
+    T getDistScale()           { return distScale; }
 
     //  Set the Dolly to a specified distance.
     //////////////////////////////////////////////////////////////////
@@ -512,10 +526,17 @@ private:
     tVec3 pan   = tVec3(T(0));
     tVec3 dolly = tVec3(T(0));
 
-    T povPanDollyFactor = T(0);
-    T dollyScale = T(.01);  //dolly scale
-    T panScale   = T(.01);  //pan scale
-    T wheelScale = T(5);   //dolly multiply for wheel
+    const T constDollyScale = T(.01);
+    const T constPanScale   = T(.01);  //pan scale
+    const T constWheelScale = T(7.5);  //dolly multiply for wheel step
+    const T constDistScale  = T( .1);  //speed by distance sensibility
+
+    T dollyScale = constDollyScale;  //dolly scale
+    T panScale   = constPanScale  ;  //pan scale
+    T wheelScale = constWheelScale;  //dolly multiply for wheel step
+    T distScale  = constDistScale ;  //speed by distance sensibility
+
+    T povPanDollyFactor = T(0); // internal use, maintain memory of current distance (pan/zoom speed by distance)
 };
 
 #ifdef VGM_USES_TEMPLATE
