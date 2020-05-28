f5acb40d63e4eafd792caf84f0bd62c8893b775e
BrutPitt <brutpitt@gmail.com>
2020-05-28 17:31:34 +0200

    ver. 1.5 Release (WiP) - build operations: output according with RAMDISK environment variable

--- src/emsCMakeGen.sh
+++ src/emsCMakeGen.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-if [ -z "$EMSCRIPTEN" ] && [ "$#" -lt 1 ]; then
+if [ "$#" -lt 3 ]; then
     echo ""
-    echo "Please set EMSCRIPTEN environment var, or pass location via command line"
-    echo "    for example: $0 /emsdk/emscripten/1.38.8"
-    echo "             or: $0 /emsdk/emscripten/1.38.8 [Debug|RelWithDebInfo|Release|MinSizeRel]"
-    echo "                  \"MinSizeRel\" is default build option"
-    echo "                  \"emsOut\" is default directory"
+    echo "Please use $0 /emsdk/emscripten/1.38.8 Debug|RelWithDebInfo|Release|MinSizeRel wglChAoSP|wglChAoSP_lowres"
     echo ""
 else    
-    if [ "$#" -gt 1 ]; then
         EMSCRIPTEN=$1
-    fi        
-
-    if [ "$#" -eq 2 ]; then
         BUILD_TYPE=$2
-    else
-        BUILD_TYPE='MinSizeRel'
-    fi
+        PROG_TYPE=$3
 
 
     EMS_DIR=$RAMDISK"builds/ems"$BUILD_TYPE
 
-    cmake -DCMAKE_TOOLCHAIN_FILE:STRING=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DBUILD_wglChAoSp:BOOL=TRUE -G "Ninja" -B$EMS_DIR
+    cmake -DCMAKE_TOOLCHAIN_FILE:STRING=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE -DBUILD_$PROG_TYPE:BOOL=TRUE -G "Ninja" -B$EMS_DIR
 
     cd $EMS_DIR
     ninja
