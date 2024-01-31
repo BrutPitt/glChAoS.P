8cfbe7c7f3913dcc94c0fd62300a5d31711386a3
BrutPitt <brutpitt@gmail.com>
2024-01-31 20:04:12 +0100

    ver 1.7 WiP - Changed copyright (date and personal infos)

--- Shaders/shadowFrag.glsl
+++ Shaders/shadowFrag.glsl
@@ -1,20 +1,16 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (c) 2018-2020 Michele Morrone
+//------------------------------------------------------------------------------
+//  Copyright (c) 2018-2024 Michele Morrone
 //  All rights reserved.
 //
-//  mailto:me@michelemorrone.eu
-//  mailto:brutpitt@gmail.com
-//  
-//  https://github.com/BrutPitt
+//  https://michelemorrone.eu - https://glchaosp.com - https://brutpitt.com
 //
-//  https://michelemorrone.eu
-//  https://BrutPitt.com
+//  X: https://x.com/BrutPitt - GitHub: https://github.com/BrutPitt
+//
+//  direct mail: brutpitt(at)gmail.com - me(at)michelemorrone.eu
 //
 //  This software is distributed under the terms of the BSD 2-Clause license
-//  
-////////////////////////////////////////////////////////////////////////////////
-#line 17    //#version dynamically inserted
+//------------------------------------------------------------------------------
+#line 13    //#version dynamically inserted
 
 in vec4 mvVtxPos;
 in vec4 solidVtx;
