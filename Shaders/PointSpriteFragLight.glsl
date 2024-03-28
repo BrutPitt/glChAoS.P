8ffdce73b4dae05f9ec1663ba01515abda9056e4
BrutPitt <brutpitt@gmail.com>
2024-03-28 20:30:20 +0100

    ver 1.7 WiP - Adding "discard" for invisible particles and Changed copyright (date and personal infos)

--- Shaders/PointSpriteFragLight.glsl
+++ Shaders/PointSpriteFragLight.glsl
@@ -1,21 +1,21 @@
 //------------------------------------------------------------------------------
-//  Copyright (c) 2018-2020 Michele Morrone
+//  Copyright (c) 2018-2024 Michele Morrone
 //  All rights reserved.
 //
-//  https://michelemorrone.eu - https://BrutPitt.com
+//  https://michelemorrone.eu - https://glchaosp.com - https://brutpitt.com
 //
-//  twitter: https://twitter.com/BrutPitt - github: https://github.com/BrutPitt
+//  X: https://x.com/BrutPitt - GitHub: https://github.com/BrutPitt
 //
-//  mailto:brutpitt@gmail.com - mailto:me@michelemorrone.eu
-//  
+//  direct mail: brutpitt(at)gmail.com - me(at)michelemorrone.eu
+//
 //  This software is distributed under the terms of the BSD 2-Clause license
 //------------------------------------------------------------------------------
-#line 14    //#version dynamically inserted
+#line 13    //#version dynamically inserted
 
 void main()
 {
 
     vec2 ptCoord = vec2(gl_PointCoord.x,1.0-gl_PointCoord.y); //upsideDown: revert point default
     outColor = mainFunc(ptCoord);
-
+    if(bool(u.slowMotion) && alphaAttenuation<.05) discard;
 }
